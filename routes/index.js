5b5cc096b37825b36e869d07eddaa87130622e1a
Lingrode <grimaloilya@gmail.com>
2024-07-30 14:46:45 -0600

    created first test route and saving images with multer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const destination = "uploads";
+
+// Shows where files must be saved
+const storage = multer.diskStorage({
+  destination: destination,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const uploads = multer({ storage: storage });
+
+router.get("/register", (req, res) => {
+  res.send("Register");
 });
 
 module.exports = router;
